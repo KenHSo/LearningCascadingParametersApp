dd20e3046dd76dd3235a19c262a8409f8d3010cc
Kenneth Haagen Sørensen <khso63309@edu.ucl.dk>
2025-03-14 14:07:23 +0100

    Add project files.

---
+++ CascadingParametersApp/Models.cs
@@ -0,0 +1,18 @@
+﻿namespace CascadingParametersApp
+{
+    public class Models
+    {
+        public class ThemeSettings
+        {
+            public string PrimaryColor { get; set; }
+            public bool IsDarkMode { get; set; }
+        }
+
+        public class UserInfo
+        {
+            public string Username { get; set; }
+            public string Role { get; set; }
+        }
+
+    }
+}
